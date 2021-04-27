9a81cd48eb95d1cbe7aeba2910c88579cff1304c
Tyoma Zagoskin <artyzago@gmail.com>
2021-04-27 23:24:43 +0500

    Initial commit

---
+++ FlightsSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlightsSearch
+//
+//  Created by Artyom Zagoskin on 27.04.2021.
+//
+import Vision
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
